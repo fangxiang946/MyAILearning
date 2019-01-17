0955e9634907b583e1370f4a1a82619cec5d14fc
fangxiang <fangxiang@beisen.com>
2019-01-17 17:44:13 +0800

    beisen

---
+++ notebook/beisen/测试插入租户数据.sql
@@ -0,0 +1,7 @@
+use BeisenRecruitmentCommon
+truncate table TenantInfoMini
+insert into TenantInfoMini (TenantId,TenantName,ChannelId,PortalId,SiteDomain,MicroSiteDomain) values (100216,'独立授权系统管理',560000108,10021604,'sysmanage001.zhuanye.cn','100216test.wechatdevmzhiye.beisencorp.com')
+insert into TenantInfoMini (TenantId,TenantName,ChannelId,PortalId,SiteDomain,MicroSiteDomain) values (101126,'bisettest001',560000213,101126,'bisettest001.zhuanye.cn','101126test.wechatdevmzhiye.beisencorp.com')
+insert into TenantInfoMini (TenantId,TenantName,ChannelId,PortalId,SiteDomain,MicroSiteDomain) values (100082,'recruitaccount',560000063,10008200,'recruitaccount10008200.zhuanye.cn',null)
+insert into TenantInfoMini (TenantId,TenantName,ChannelId,PortalId,SiteDomain,MicroSiteDomain) values (100082,'recruitaccount',560000065,10008202,null,'recruitaccountb.wechatdevmzhiye.beisencorp.com')
+insert into TenantInfoMini (TenantId,TenantName,ChannelId,PortalId,SiteDomain,MicroSiteDomain) values (100082,'recruitaccount',560000103,10008201,'recruitaccounta.zhuanye.cn','recruitaccounta.wechatdevmzhiye.beisencorp.com')
